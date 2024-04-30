cf155a2c315fadf13f3a6f44dd1df2e89a24c289
Shubh Udhani <117093207+Shubh8604@users.noreply.github.com>
2024-04-30 12:08:51 +0530

    Task4

--- Task4.cpp
+++ Task4.cpp
@@ -1,235 +1,204 @@
-#include <iostream>
-#include <fstream>
-#include<string>
-#include <cstdlib>
-
+#include<iostream>
 using namespace std;
-int ID;
-//custom type todo which has two fields id and task
-struct todo {
-    int id;
-    string task;
+
+const int MAX_SIZE=500;
+
+
+struct Task{
+	
+	string task;
+	string status;
+	
+	
 };
 
-//this method is used to add a new task to the list of tasks
-void addtodo() {
-    system("cls");
-    cout<<"\t\t\t***********************************************************************"<<endl;
-	cout<<"\t\t\t                       WELCOME TO Your ToDo List                       "<<endl;
-    cout<<"\t\t\t***********************************************************************"<<endl<<endl<<endl;
-    todo todo;
-    cout << "\n\tEnter new task: ";
-    cin.get();
-    getline(cin, todo.task); //get user input
-    ID++; //increment id for the current task
+class ToDoList{
+	
+    private:
+		Task taskArray[MAX_SIZE];
+		int no_task;
 
-    //now write this task to the todo.txt file
-    ofstream write;
-    write.open("todo.txt", ios::app);
-    write << "\n" << ID;
-    write << "\n" << todo.task ;
-    write.close();
+		
+    public:
 
-    //write the id to a new file so that we can use this id later to add new task
-    write.open("id.txt");
-    write << ID;
-    write.close();
+	ToDoList(){
+		
+	
+		no_task=1;
+		
+	}
+	
+	
+	void addTask(string task_info){
+	    
+	    taskArray[no_task].task=task_info;
+	    	
+	    no_task++;
+	    	
+	    cout<<"Your Task is Added Successfully "<<endl;
+	
 
-    char ch;
-    cout<<"Do you want to add more task? y/n"<<endl;
-    cin>> ch;
-
-    //if user wants to add a new task again then call the same function else return
-    if(ch == 'y'){
-        addtodo();
-    }
-    else{
-        cout << "\n\tTask has been added successfully";
-        return;
-    }
 }
 
-// this method is used to print the task on the screen
-void print(todo s) {
-    cout << "\n\tID is : " << s.id;
-    cout << "\n\tTask is : " << s.task;
+
+    void editTask(int id, string edit){
+	
+		if(id>0 && id <= no_task){
+			taskArray[id].task=edit;
+		}
+		else{
+			cout<<"INVALID INPUT"<<endl;
+		}
 }
 
-//This method is used to read data from the todo.txt file and print it on screen
-void readData() {
-    system("cls");
-     cout<<"\t\t\t***********************************************************************"<<endl;
-	cout<<"\t\t\t                       WELCOME TO Your ToDo List                       "<<endl;
-    cout<<"\t\t\t***********************************************************************"<<endl<<endl<<endl;
-    todo todo;
-    ifstream read;
-    read.open("todo.txt");
-    cout << "\n\t------------------Your current Tasks in the list--------------------";
-    // while we dont reach the end of file keep on printing the data on screen
-    while (!read.eof()) {
-        read >> todo.id;
-        read.ignore();
-        getline(read, todo.task);
-        print(todo);
-    }
-    read.close();
+
+void deleteTask(int id){
+	
+	
+	if(id>0 && id<=no_task){
+		
+		for(int i=id; i<no_task-1; ++i){
+			taskArray[id]= taskArray[i+1];
+			
+			
+		}
+		no_task--;
+
+	}
+	else{
+		cout<<"INVALID INPUT !!"<<endl;
+	}
 }
 
-//this method is used to search for a specific task from the todo.txt file
-int searchData() {
-    system("cls");
-     cout<<"\t\t\t***********************************************************************"<<endl;
-	cout<<"\t\t\t                       WELCOME TO Your ToDo List                       "<<endl;
-    cout<<"\t\t\t***********************************************************************"<<endl<<endl<<endl;
-    int id;
-    cout << "\n\tEnter task id: ";
-    cin >> id;
-    todo todo;
-    ifstream read;
-    read.open("todo.txt");
-    //while we dont reach end of file keep or searching for the id to match to the user input id
-    while (!read.eof()) {
-        read >> todo.id;
-        read.ignore();
-        getline(read, todo.task);
-        if (todo.id == id) {
-            print(todo);
-            return id;
-        }
-    }
-}
+    void DisplayList(){
+	
+	cout<<"\t\t\t\tTO DO LIST "<<endl;
+	cout<<"\t\t--------------------------------------------------"<<endl;
+		for(int i=1;i<no_task;i++){
+				cout<<"\t\tTASKS  \t       STATUS\t\t  DESCRIPTION"<<endl;
+				cout<<"\t\t--------------------------------------------------"<<endl;
+				cout<<"\t\t"<<i<<"\t     "<<taskArray[i].status<<"\t\t\t"<<taskArray[i].task<<"\n\n\n";
+				
+			
+			}
+	
+}	
 
-// this method is used to delete the task from the todo.txt file
-void deleteData() {
-    system("cls");
-    cout<<"\t\t\t***********************************************************************"<<endl;
-	cout<<"\t\t\t                       WELCOME TO Your ToDo List                       "<<endl;
-    cout<<"\t\t\t***********************************************************************"<<endl<<endl<<endl;
-    int id = searchData();
-    cout << "\n\tDo you want to delete this task (y/n) : ";
-    char choice;
-    cin >> choice;
-    if (choice == 'y') {
-        todo todo;
-        ofstream tempFile;
-        tempFile.open("temp.txt");
-        ifstream read;
-        read.open("todo.txt");
-        //while we dont reach the end of file keep on searching for the id to delete the task
-        while (!read.eof()) {
-            read >> todo.id;
-            read.ignore();
-            getline(read, todo.task);
-            if (todo.id != id) {
-                tempFile << "\n" << todo.id;
-                tempFile << "\n" << todo.task;
-            }
-        }
-        read.close();
-        tempFile.close();
-        remove("todo.txt");
-        rename("temp.txt", "todo.txt");
-        cout << "\n\tTask deleted successfuly";
-    }
-    else {
-        cout << "\n\tRecord not deleted";
-    }
-}
 
-//this method is used to update the task
-//here we create a new temp.txt file and add all the updated data to this file
-//once updated we then delete the original todo.txt and then rename this file to todo.txt
-void updateData() {
-    system("cls");
-    cout<<"\t\t\t***********************************************************************"<<endl;
-	cout<<"\t\t\t                       WELCOME TO Your ToDo List                       "<<endl;
-    cout<<"\t\t\t***********************************************************************"<<endl<<endl<<endl;
-    int id = searchData();
-    cout << "\n\tYou want to update this task (y/n) : ";
-    char choice;
-    cin >> choice;
-    if (choice == 'y') {
-        todo newData;
-        cout << "\n\tEnter todo task : ";
-        cin.get();
-        getline(cin, newData.task);
-        todo todo;
-        ofstream tempFile;
-        tempFile.open("temp.txt");
-        ifstream read;
-        read.open("todo.txt");
-        //while we dont reach end of file keep on searching for the id and once found update with new data
-        while (!read.eof()) {
-            read >> todo.id;
-            read.ignore();
-            getline(read, todo.task);
-            if (todo.id != id) {
-                tempFile << "\n" << todo.id;
-                tempFile << "\n" << todo.task;
-            }
-            else {
-                tempFile << "\n"<< todo.id;
-                tempFile << "\n"<< newData.task;
-            }
-        }
-        read.close();
-        tempFile.close();
-        remove("todo.txt");
-        rename("temp.txt", "todo.txt");
-        cout << "\n\tTask updated successfuly";
-    }
-    else {
-        cout << "\n\tRecord not deleted";
-    }
-}
-int main()
-{
-    system("cls");
-	system("Color E0"); // "Color XY", X - backgroung color, Y - text color
-    system("title todoapp @copyassignment");
-    cout<<"\t\t\t***********************************************************************"<<endl;
-    cout<<"\t\t\t*                                                                     *"<<endl;
-	cout<<"\t\t\t*                      WELCOME TO Your ToDo List                      *"<<endl;
-	cout<<"\t\t\t*                                                                     *"<<endl;
-    cout<<"\t\t\t***********************************************************************"<<endl<<endl<<endl<<endl;
-    ifstream read;
-    read.open("id.txt");
-    if (!read.fail()) {
-        read >> ID;
-    }
-    else {
-        ID = 0;
-    }
-    read.close();
+    void CompletionStatus(int id){
+	
+	if(id>0 && id <= no_task){
+		
+			taskArray[id].status="completed";
+		
+			cout<<"Task marked as Done"<<endl;
 
-    while (true) {
-        cout<<endl<<endl;
-        cout << "\n\t1.Add student data";
-        cout << "\n\t2.See student data";
-        cout << "\n\t3.Search student data";
-        cout << "\n\t4.Delete student data";
-        cout << "\n\t5.Update student data";
+	}
+	else{
+			cout<<"Invalid ID "<<endl;
+			
+		}
+	
+}		
+};
 
-        int choice;
-        cout << "\n\tEnter choice : ";
-        cin >> choice;
-        switch (choice) {
-        case 1:
-            addtodo();
-            break;
-        case 2:
-            readData();
-            break;
-        case 3:
-            searchData();
-            break;
-        case 4:
-            deleteData();
-            break;
-        case 5:
-            updateData();
-            break;
-        }
-    }
+int main(){
+	
+	ToDoList list;
+	cout<<"\n\t\t\tTO DO LIST"<<endl;
+	
+	// variables declaration
+		int choice=1,id;
+		string TASK,confirm;
+		
+	do{
+	//menu driven program
+	cout<<"\n\n";
+	cout<<"Press 1 to Add task "<<endl;
+	cout<<"Press 2 to Delete task "<<endl;
+	cout<<"Press 3 to Edit task"<<endl;
+	cout<<"Press 4 to mark done  task"<<endl;
+	cout<<"Press 5 to Display List "<<endl;
+	cout<<"Press 0 to Exit"<<endl;
+	cout<<"\nEnter your choice :"<<endl;
+	
+	cin>>choice;
+	
+	switch(choice){
+		
+		case 0 :
+			exit(0);
+		
+			
+		case 1 :
+			
+			cout<<"\nEnter your task : "<<endl;
+		    cin.get();
+			getline(cin,TASK);
+			
+			list.addTask(TASK);
+			do{
+				cout<<"Want to Add more ?\n Press y for Yes OR Press n for No"<<endl;
+	        cin>>confirm;
+	        if(confirm == "y"){
+	        
+			cout<<"Enter your task : "<<endl;
+			cin.get();
+			getline(cin,TASK);
+		    list.addTask(TASK);
+		    
+	        }
+		}while(confirm!="n");
+			
+	       
+		break;
+		
+		
+		case 2 :
+			
+			cout<<"\nEnter task id which you want to Delete :"<<endl;
+			cin>>id;
+			cout<<"Confirm (y/n)?"<<endl;
+			cin>>confirm;
+			if(confirm=="y"){
+				list.deleteTask(id);
+				cout<<"Your task is Deleted Successfully !!"<<endl;
+			}
+			
+			
+		break;
+			
+			
+		case 3 :
+			
+			cout<<"\nEnter task id which you want to Edit "<<endl;
+			cin>>id;
+			cout<<"Enter New description "<<endl;
+			cin.get();
+			getline(cin,TASK);
+			cout<<"Confirm & save (y/n)?"<<endl;
+			cin>>confirm;
+			if(confirm=="y"){
+				list.editTask(id,TASK);
+				cout<<"Your task is Updated Successfully !!"<<endl;
+			}
 
+			break;
+			
+		case 4 :
+			
+			cout<<"\nEnter the task ID which you want to mark as done :"<<endl;
+			cin>>id;
+			list.CompletionStatus(id);
+			break;
+
+		case 5:
+			
+			list.DisplayList();
+			break;
+
+			
+	}
+	}while(choice!=0);
+	
 }
